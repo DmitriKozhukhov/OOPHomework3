78249697ef66d236f7ae763854895dfa997ce9e0
Dmitri Kozhukhov <dmitrikozhukhov@gmail.com>
2024-02-15 12:50:37 +0300

    Eng version pluged

--- src/main/java/org/example/EnGame.java
+++ src/main/java/org/example/EnGame.java
@@ -1,4 +1,17 @@
 package org.example;
 
-public class EnGame {
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnGame extends  AbstractGame{
+
+    @Override
+    public List<String> generateCharList() {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 'a'; i <= 'z' ; i++) {
+            result.add(String.valueOf((char) i));
+        }
+        return result;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractGame nGame = new RuGame();
+        //
         Scanner scanner = new Scanner(System.in);
+        AbstractGame nGame;
 
+        while(true){
+            System.out.print("Выберите тип игры: \n" +
+                    "1 - Игра на русском \n" +
+                    "2 - Игра на английском");
+            if(scanner.nextLine().equals("1")){
+                nGame = new RuGame();
+                break;
+            }
+            if(scanner.nextLine().equals("2")){
+                nGame = new EnGame();
+                break;
+            }
+        }
         nGame.start(4, 10);
         while(nGame.getGameStatus().equals(GameStatus.START)){
             System.out.print("Ваш ход: ");
